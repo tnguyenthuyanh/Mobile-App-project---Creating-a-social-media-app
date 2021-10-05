8c61d131d0d20a268ef7994c7c712e3d0bc312f0
thu <tnguyenthuyanh@uco.edu>
2021-10-05 01:55:44 -0500

    progressing

---
+++ lib/controller/firebaseauth_controller.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthController {
+  static Future<User?> signIn({required String email, required String password}) async {
+    UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+    return userCredential.user;
+  }
+}
--- lib/viewscreen/signin_screen.dart
+++ lib/viewscreen/signin_screen.dart
@@ -10,13 +10,96 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInState extends State<SignInScreen> {
+  late _Controller con;
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    con = _Controller(this);
+  }
+
+  void render(fn) => setState(fn);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sign In'),
       ),
-      body: Text(''),
+      body: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                'Sign In, Please!',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email address',
+                ),
+                keyboardType: TextInputType.emailAddress,
+                autocorrect: false,
+                validator: con.validateEmail,
+                onSaved: con.saveEmail,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Enter password',
+                ),
+                obscureText: true,
+                autocorrect: false,
+                validator: con.validatePassword,
+                onSaved: con.savePassword,
+              ),
+              ElevatedButton(
+                onPressed: con.signIn,
+                child:
+                    Text('Sign In', style: Theme.of(context).textTheme.button),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+}
+
+class _Controller {
+  late _SignInState state;
+  _Controller(this.state);
+  String? email;
+  String? password;
+
+  String? validateEmail(String? value) {
+    if (value == null || !(value.contains('.') && value.contains('@')))
+      return 'Invalid email address';
+    else
+      return null;
+  }
+
+  void saveEmail(String? value) {
+    if (value != null) email = value;
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.length < 6)
+      return 'Invalid password';
+    else
+      return null;
+  }
+
+  void savePassword(String? value) {
+    if (value != null) password = value;
+  }
+
+  void signIn() {
+    FormState? currentState = state.formKey.currentState;
+    if (currentState == null || !currentState.validate()) return;
+
+    currentState.save();
+
   }
 }
